fc59cb3ba44bb58389aaee23bb8a8ecd4e5a93d9
camerajunkie <ebenofthecamera@gmail.com>
2025-05-31 11:08:38 -0400

    feat(playwright.config.ts): implement playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './playwright', // Only run Playwright tests in this directory
+  // You can add more Playwright options here if needed
+}); 
